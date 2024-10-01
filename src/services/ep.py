2286a23a942967126973a275af1b157e6ce8431c
Giuliano Poeta <giuliano.poeta.laboral@gmail.com>
2024-10-01 10:49:47 -0300

    se arreglo ejecucion del tip en el y srtn

--- src/services/ep.py
+++ src/services/ep.py
@@ -23,15 +23,15 @@ class ExternalPriority(PolicyStrategy):
 		self.system_executor = System()
 		self.update_io_blocked_queue()
 		self.update_ready_queue()
-		self.sort_ready_queue(1)
+		self.sort_ready_queue()
 
 	def advance_time_unit(self):
 		self.logger.log_ws()
 		self.time_unit += 1
 		
-	def sort_ready_queue(self, donde):
+	def sort_ready_queue(self):
 		self.ready_queue.sort(key=lambda process: process.priority, reverse=True)
-		self.logger.info(self.time_unit, f'{donde}', f"Ready Queue sorted by priority: {[p.id for p in self.ready_queue]}")
+		self.logger.info(self.time_unit, '-', f"Ready Queue sorted by priority: {[p.id for p in self.ready_queue]}")
 
 	def update_io_blocked_queue(self):
 		# contador de procesos revisados
@@ -79,25 +79,13 @@ class ExternalPriority(PolicyStrategy):
 					cpu_burst_count=process.cpu_burst_count - 1 if process.cpu_burst_count > 0 else 0,
 					io_burst_count=process.io_burst_count
 				)
-				# ejecutamos la cantidad tip que diga en los parametros.
-				for _ in range(self.scheduler.tip):
-					# ejecucion de sistema para resumen
-					self.system_executor.execute_tip_tick(ready_process, self.time_unit)
-					self.result.os_cpu_time += 1
-					# avanzamos el tiempo de la simulacion
-					self.advance_time_unit()
-					self.logger.info(self.time_unit, 'RUNNING', f"Process '{ready_process.name}' (pid: {ready_process.id}) executing TIP")
-					# actualizamos la cola de bloqueadaos para que se ejecute el io y se muevan a la cola de listos
-					self.update_io_blocked_queue()
-					# revisamos que no hayan aprecido nuevos procesos
-					self.update_ready_queue()
-				# informamos que se ejecuto el tip
-				self.logger.info(self.time_unit, 'READY', f"Process '{process.name}' (pid: {process.id}) was added to Ready Queue")
-				ready_process.tip_already_executed = True
-				# cambiamos el id del ultimo proceso ejeutado
-				self.last_executed_process = ready_process
-
 				self.ready_queue.append(ready_process)
+
+	def all_tips_executed(self):
+		for process in self.ready_queue:
+			if not process.tip_already_executed:
+				return False
+		return True
 
 	def execute(self):
 		# ejecutar la simulacion hasta que los procesos finalizados sean iguales a los procesoso totales
@@ -115,106 +103,128 @@ class ExternalPriority(PolicyStrategy):
 				# actualizamos la cola de listos
 				self.update_ready_queue()
 				# ordenamos la cola de listos
-				self.sort_ready_queue(3)
+				self.sort_ready_queue()
 			else:
-				# sacamos el siguiente proceso de la cola
-				process = self.ready_queue[0]
-				if (
-					process != self.last_executed_process and 
-					self.last_executed_process != None and 
-					self.last_executed_process.burst_in_execution 
-				):
-					# ejecutamos el tcp porque se interrumpio el proceso anterior
-					for _ in range(self.scheduler.tcp):
-						# ejecutamos estadisticas de para el resumen
-						self.system_executor.execute_tcp_tick(self.last_executed_process, self.time_unit)
-						self.result.os_cpu_time += 1
-						# avanzamos el tiempo de la simulacion
-						self.advance_time_unit()
-						self.logger.info(self.time_unit, 'RUNNING', f"Process '{self.last_executed_process.name}' (pid: {self.last_executed_process.id}) executing TCP")
-						# actualizamos los bloqueados ejecutando y moviendolos a listo
-						self.update_io_blocked_queue()
-						# revisamos que no hayan procesos nuevos
-						self.update_ready_queue()
-					# cambiamos el id del ultimo proceso ejeutado
-					self.logger.info(self.time_unit, 'RUNNING', f"Process '{self.last_executed_process.name}' (pid: {self.last_executed_process.id}) was interrupted by process '{process.name}' (pid: {process.id})")
-					self.last_executed_process = process
+				if not self.all_tips_executed():
+					for ready_process in self.ready_queue:
+						if not ready_process.tip_already_executed:
+							# ejecutamos la cantidad tip que diga en los parametros.
+							for _ in range(self.scheduler.tip):
+								# ejecucion de sistema para resumen
+								self.system_executor.execute_tip_tick(ready_process, self.time_unit)
+								self.result.os_cpu_time += 1
+								# avanzamos el tiempo de la simulacion
+								self.advance_time_unit()
+								self.logger.info(self.time_unit, 'RUNNING', f"Process '{ready_process.name}' (pid: {ready_process.id}) executing TIP")
+								# actualizamos la cola de bloqueadaos para que se ejecute el io y se muevan a la cola de listos
+								self.update_io_blocked_queue()
+								# revisamos que no hayan aprecido nuevos procesos
+								self.update_ready_queue()
+							# informamos que se ejecuto el tip
+							ready_process.tip_already_executed = True
+							# cambiamos el id del ultimo proceso ejeutado
+							self.last_executed_process = ready_process
+							# ordenamos la cola de listos
+							self.sort_ready_queue()
 				else:
-					# preguntamos si le quedan rafagas pendientes al proceso actual
-					if process.pending_cpu_burst_in_execution > 0:
-						# ejecutamos estadisticas de para el resumen
-						self.system_executor.execute_cpu_tick(process, self.time_unit)
-						# ejecutamos la rafaga del proceso
-						process.service_time += 1
-						for i in range(1, len(self.ready_queue)):
-							self.ready_queue[i].ready_wait_time += 1
-						process.burst_in_execution = True
-						# cambiamos la info del ultimo proceso ejecutado
+					# sacamos el siguiente proceso de la cola
+					process = self.ready_queue[0]
+					if (
+						process != self.last_executed_process and 
+						self.last_executed_process != None and 
+						self.last_executed_process.burst_in_execution 
+					):
+						# ejecutamos el tcp porque se interrumpio el proceso anterior
+						for _ in range(self.scheduler.tcp):
+							# ejecutamos estadisticas de para el resumen
+							self.system_executor.execute_tcp_tick(self.last_executed_process, self.time_unit)
+							self.result.os_cpu_time += 1
+							# avanzamos el tiempo de la simulacion
+							self.advance_time_unit()
+							self.logger.info(self.time_unit, 'RUNNING', f"Process '{self.last_executed_process.name}' (pid: {self.last_executed_process.id}) executing TCP")
+							# actualizamos los bloqueados ejecutando y moviendolos a listo
+							self.update_io_blocked_queue()
+							# revisamos que no hayan procesos nuevos
+							self.update_ready_queue()
+						# cambiamos el id del ultimo proceso ejeutado
+						self.logger.info(self.time_unit, 'RUNNING', f"Process '{self.last_executed_process.name}' (pid: {self.last_executed_process.id}) was interrupted by process '{process.name}' (pid: {process.id})")
 						self.last_executed_process = process
-						# avanzamos el tiempo de la simulacion
-						self.advance_time_unit()
-						self.logger.info(self.time_unit, 'RUNNING', f"Process '{process.name}' (pid: {process.id}) executing CPU burst")
-						# actualizamos los bloqueados ejecutando y moviendolos
-						self.update_io_blocked_queue()
-						# revisamos que no hayan procesos nuevos
-						self.update_ready_queue()
-						# ordenamos la cola de listos
-						self.sort_ready_queue(4)
+					else:
+						# preguntamos si le quedan rafagas pendientes al proceso actual
+						if process.pending_cpu_burst_in_execution > 0:
+							# ejecutamos estadisticas de para el resumen
+							self.system_executor.execute_cpu_tick(process, self.time_unit)
+							# ejecutamos la rafaga del proceso
+							process.service_time += 1
+							for i in range(1, len(self.ready_queue)):
+								self.ready_queue[i].ready_wait_time += 1
+							process.burst_in_execution = True
+							# cambiamos la info del ultimo proceso ejecutado
+							self.last_executed_process = process
+							# avanzamos el tiempo de la simulacion
+							self.advance_time_unit()
+							self.logger.info(self.time_unit, 'RUNNING', f"Process '{process.name}' (pid: {process.id}) executing CPU burst")
+							# actualizamos los bloqueados ejecutando y moviendolos
+							self.update_io_blocked_queue()
+							# revisamos que no hayan procesos nuevos
+							self.update_ready_queue()
+							# ordenamos la cola de listos
+							self.sort_ready_queue()
 
-					# pregunamos si ya no le queda mas ejecucion de la rafaga actual
-					if process.pending_cpu_burst_in_execution == 0:
-						# preguntaos si le quedan mas bloques de rafagas pendientes
-						if process.cpu_burst_count > 0:
-							process.cpu_burst_count -= 1
-							process.pending_cpu_burst_in_execution = process.cpu_burst_duration
-							process.burst_in_execution = False
-							process.burst_in_execution_finish = True
-							# ejecutamos el tcp porque el proceso termino su rafaga y pasa a la cola de bloqueados
-							for _ in range(self.scheduler.tcp):
-								# ejecutamos estadisticas de para el resumen
-								self.system_executor.execute_tcp_tick(process, self.time_unit)
-								self.result.os_cpu_time += 1
-								# avanzamos el tiempo de la simulacion
-								self.advance_time_unit()
-								self.logger.info(self.time_unit, 'RUNNING', f"Process '{process.name}' (pid: {process.id}) executing TCP")
-								# actualizamos los bloqueados ejecutando y moviendolos a listo
-								self.update_io_blocked_queue()
-								# revisamos que no hayan procesos nuevos
-								self.update_ready_queue()
-							# una vez que termina el tcp movemos el proceso a la cola de bloqueados
-							self.io_blocked_queue.append(process)
-							# y sacamos el proceso de la cola
-							self.ready_queue.remove(process)
-							self.logger.info(self.time_unit, 'RUNNING', f"Process '{process.name}' (pid: {process.id}) moved to I/O Blocked Queue")
-							# ordenamos la cola de listos
-							self.sort_ready_queue(5)
-						elif process.cpu_burst_count == 0:
-							process.burst_in_execution = False
-							process.burst_in_execution_finish = True
-							# ejecutamos el tfp porque ya no hay rafagas pendientes
-							for _ in range(self.scheduler.tfp):
-								# ejecutamos estadisticas de para el resumen
-								self.system_executor.execute_tfp_tick(process, self.time_unit)
-								self.result.os_cpu_time += 1
-								# avanzamos el tiempo de la simulacion
-								self.advance_time_unit()
-								self.logger.info(self.time_unit, 'RUNNING', f"Process '{process.name}' (pid: {process.id}) executing TFP")
-								# actualizamos los bloqueados ejecutando y moviendolos a listo
-								self.update_io_blocked_queue()
-								# revisamos que no hayan procesos nuevos
-								self.update_ready_queue()
-							# finalizamos el proceso agragandolo a la cola de finalizados
-							self.result.finished_processes.append(FinishedProcess(
-								**process.__dict__,
-								return_instant=self.time_unit,
-								return_time=self.time_unit - process.arrival_time,
-								normalized_return_time=(self.time_unit - process.arrival_time) / process.service_time
-							))
-							# lo sacamos de la cola de listos
-							self.ready_queue.remove(process)
-							self.logger.info(self.time_unit, 'FINISHED', f"Process '{process.name}' (pid: {process.id}) finished")
-							# ordenamos la cola de listos
-							self.sort_ready_queue(6)
+						# pregunamos si ya no le queda mas ejecucion de la rafaga actual
+						if process.pending_cpu_burst_in_execution == 0:
+							# preguntaos si le quedan mas bloques de rafagas pendientes
+							if process.cpu_burst_count > 0:
+								process.cpu_burst_count -= 1
+								process.pending_cpu_burst_in_execution = process.cpu_burst_duration
+								process.burst_in_execution = False
+								process.burst_in_execution_finish = True
+								# ejecutamos el tcp porque el proceso termino su rafaga y pasa a la cola de bloqueados
+								for _ in range(self.scheduler.tcp):
+									# ejecutamos estadisticas de para el resumen
+									self.system_executor.execute_tcp_tick(process, self.time_unit)
+									self.result.os_cpu_time += 1
+									# avanzamos el tiempo de la simulacion
+									self.advance_time_unit()
+									self.logger.info(self.time_unit, 'RUNNING', f"Process '{process.name}' (pid: {process.id}) executing TCP")
+									# actualizamos los bloqueados ejecutando y moviendolos a listo
+									self.update_io_blocked_queue()
+									# revisamos que no hayan procesos nuevos
+									self.update_ready_queue()
+								# una vez que termina el tcp movemos el proceso a la cola de bloqueados
+								self.io_blocked_queue.append(process)
+								# y sacamos el proceso de la cola
+								self.ready_queue.remove(process)
+								self.logger.info(self.time_unit, 'RUNNING', f"Process '{process.name}' (pid: {process.id}) moved to I/O Blocked Queue")
+								# ordenamos la cola de listos
+								self.sort_ready_queue()
+							elif process.cpu_burst_count == 0:
+								process.burst_in_execution = False
+								process.burst_in_execution_finish = True
+								# ejecutamos el tfp porque ya no hay rafagas pendientes
+								for _ in range(self.scheduler.tfp):
+									# ejecutamos estadisticas de para el resumen
+									self.system_executor.execute_tfp_tick(process, self.time_unit)
+									self.result.os_cpu_time += 1
+									# avanzamos el tiempo de la simulacion
+									self.advance_time_unit()
+									self.logger.info(self.time_unit, 'RUNNING', f"Process '{process.name}' (pid: {process.id}) executing TFP")
+									# actualizamos los bloqueados ejecutando y moviendolos a listo
+									self.update_io_blocked_queue()
+									# revisamos que no hayan procesos nuevos
+									self.update_ready_queue()
+								# finalizamos el proceso agragandolo a la cola de finalizados
+								self.result.finished_processes.append(FinishedProcess(
+									**process.__dict__,
+									return_instant=self.time_unit,
+									return_time=self.time_unit - process.arrival_time,
+									normalized_return_time=(self.time_unit - process.arrival_time) / process.service_time
+								))
+								# lo sacamos de la cola de listos
+								self.ready_queue.remove(process)
+								self.logger.info(self.time_unit, 'FINISHED', f"Process '{process.name}' (pid: {process.id}) finished")
+								# ordenamos la cola de listos
+								self.sort_ready_queue()
 						
 
 		# una vez que termina la simulacion ordenamos la lista de finalizados por el pid y agregamso varios parametros para el resultado final de la misma
--- src/services/srtn.py
+++ src/services/srtn.py
@@ -29,11 +29,9 @@ class SRTN(PolicyStrategy):
 		self.logger.log_ws()
 		self.time_unit += 1
 		
-
 	def sort_ready_queue(self):
 		self.ready_queue.sort(key=lambda process: process.pending_cpu_burst_in_execution)
 		self.logger.info(self.time_unit, '-', f"Ready Queue sorted by remaining time: {[p.id for p in self.ready_queue]}")
-
 
 	def update_io_blocked_queue(self):
 		# contador de procesos revisados
@@ -63,7 +61,8 @@ class SRTN(PolicyStrategy):
 		# verificamos que no hayan llegado nuevos procesos y si llegan los agregamos al final de la cola
 		for process in self.scheduler.processes:
 			if process.arrival_time == self.time_unit:
-				self.ready_queue.append(RunningProcess(
+				self.logger.info(self.time_unit, 'READY', f"Process '{process.name}' (pid: {process.id}) has arrived")
+				ready_process = RunningProcess(
 					id=process.id,
 					name=process.name,
 					arrival_time=process.arrival_time,
@@ -79,8 +78,14 @@ class SRTN(PolicyStrategy):
 					ready_wait_time=0,
 					cpu_burst_count=process.cpu_burst_count - 1 if process.cpu_burst_count > 0 else 0,
 					io_burst_count=process.io_burst_count
-				))
-				self.logger.info(self.time_unit, 'READY', f"Process '{process.name}' (pid: {process.id}) arrived and added to Ready Queue")
+				)
+				self.ready_queue.append(ready_process)
+
+	def all_tips_executed(self):
+		for process in self.ready_queue:
+			if not process.tip_already_executed:
+				return False
+		return True
 
 	def execute(self):
 		# ejecutar la simulacion hasta que los procesos finalizados sean iguales a los procesoso totales
@@ -100,48 +105,50 @@ class SRTN(PolicyStrategy):
 				# ordenamos la cola de listos
 				self.sort_ready_queue()
 			else:
-				# sacamos el siguiente proceso de la cola
-				process = self.ready_queue[0]
-				if (
-					process != self.last_executed_process and 
-					self.last_executed_process != None and 
-					self.last_executed_process.burst_in_execution):
-					# ejecutamos el tcp porque se interrumpio el proceso anterior
-					for _ in range(self.scheduler.tcp):
-						# ejecutamos estadisticas de para el resumen
-						self.system_executor.execute_tcp_tick(self.last_executed_process, self.time_unit)
-						self.result.os_cpu_time += 1
-						# avanzamos el tiempo de la simulacion
-						self.advance_time_unit()
-						self.logger.info(self.time_unit, 'RUNNING', f"Process '{self.last_executed_process.name}' (pid: {self.last_executed_process.id}) executing TCP")
-						# actualizamos los bloqueados ejecutando y moviendolos a listo
-						self.update_io_blocked_queue()
-						# revisamos que no hayan procesos nuevos
-						self.update_ready_queue()
-					# cambiamos el id del ultimo proceso ejeutado
-					self.logger.info(self.time_unit, 'RUNNING', f"Process '{self.last_executed_process.name}' (pid: {self.last_executed_process.id}) was interrupted by process '{process.name}' (pid: {process.id})")
-					self.last_executed_process = process
+				if not self.all_tips_executed():
+					for ready_process in self.ready_queue:
+						if not ready_process.tip_already_executed:
+							# ejecutamos la cantidad tip que diga en los parametros.
+							for _ in range(self.scheduler.tip):
+								# ejecucion de sistema para resumen
+								self.system_executor.execute_tip_tick(ready_process, self.time_unit)
+								self.result.os_cpu_time += 1
+								# avanzamos el tiempo de la simulacion
+								self.advance_time_unit()
+								self.logger.info(self.time_unit, 'RUNNING', f"Process '{ready_process.name}' (pid: {ready_process.id}) executing TIP")
+								# actualizamos la cola de bloqueadaos para que se ejecute el io y se muevan a la cola de listos
+								self.update_io_blocked_queue()
+								# revisamos que no hayan aprecido nuevos procesos
+								self.update_ready_queue()
+							# informamos que se ejecuto el tip
+							ready_process.tip_already_executed = True
+							# cambiamos el id del ultimo proceso ejeutado
+							self.last_executed_process = ready_process
+							# ordenamos la cola de listos
+							self.sort_ready_queue()
 				else:
-					# si el ultimo proceso ejecutado es el mismo que el actual y no se ejecuto su tip lo ejecuto si no se ejecuta de manera normal
-					if not process.tip_already_executed:
-						# ejecutamos la cantidad tip que diga en los parametros.
-						for _ in range(self.scheduler.tip):
-							# ejecucion de sistema para resumen
-							self.system_executor.execute_tip_tick(process, self.time_unit)
+					# sacamos el siguiente proceso de la cola
+					process = self.ready_queue[0]
+					if (
+						process != self.last_executed_process and 
+						self.last_executed_process != None and 
+						self.last_executed_process.burst_in_execution 
+					):
+						# ejecutamos el tcp porque se interrumpio el proceso anterior
+						for _ in range(self.scheduler.tcp):
+							# ejecutamos estadisticas de para el resumen
+							self.system_executor.execute_tcp_tick(self.last_executed_process, self.time_unit)
 							self.result.os_cpu_time += 1
 							# avanzamos el tiempo de la simulacion
 							self.advance_time_unit()
-							self.logger.info(self.time_unit, 'RUNNING', f"Process '{process.name}' (pid: {process.id}) executing TIP")
-							# actualizamos la cola de bloqueadaos para que se ejecute el io y se muevan a la cola de listos
+							self.logger.info(self.time_unit, 'RUNNING', f"Process '{self.last_executed_process.name}' (pid: {self.last_executed_process.id}) executing TCP")
+							# actualizamos los bloqueados ejecutando y moviendolos a listo
 							self.update_io_blocked_queue()
-							# revisamos que no hayan aprecido nuevos procesos
+							# revisamos que no hayan procesos nuevos
 							self.update_ready_queue()
-						# informamos que se ejecuto el tip
-						process.tip_already_executed = True
 						# cambiamos el id del ultimo proceso ejeutado
+						self.logger.info(self.time_unit, 'RUNNING', f"Process '{self.last_executed_process.name}' (pid: {self.last_executed_process.id}) was interrupted by process '{process.name}' (pid: {process.id})")
 						self.last_executed_process = process
-						# ordenamos la cola de listos
-						self.sort_ready_queue()
 					else:
 						# preguntamos si le quedan rafagas pendientes al proceso actual
 						if process.pending_cpu_burst_in_execution > 0:
@@ -216,6 +223,8 @@ class SRTN(PolicyStrategy):
 								# lo sacamos de la cola de listos
 								self.ready_queue.remove(process)
 								self.logger.info(self.time_unit, 'FINISHED', f"Process '{process.name}' (pid: {process.id}) finished")
+								# ordenamos la cola de listos
+								self.sort_ready_queue()
 						
 
 		# una vez que termina la simulacion ordenamos la lista de finalizados por el pid y agregamso varios parametros para el resultado final de la misma
