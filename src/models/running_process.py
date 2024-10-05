8c21544a736ee35d3cf41e76b30f3e79ad3a677f
Giuliano Poeta <giuliano.poeta.laboral@gmail.com>
2024-10-05 18:49:05 -0300

    Se cambio fcfs y spn por cambio inesperado de consigna

--- src/models/running_process.py
+++ src/models/running_process.py
@@ -4,8 +4,6 @@ from . import Process
 @dataclass
 class RunningProcess(Process):
     tip_already_executed: bool
-    burst_in_execution: bool
-    burst_in_execution_finish: bool
     pending_cpu_burst_in_execution: int
     pending_io_burst_in_execution: int
     service_time: int
--- src/models/scheduler_result.py
+++ src/models/scheduler_result.py
@@ -12,9 +12,19 @@ class SchedulerResult:
     os_cpu_time: int
 
     def __str__(self) -> str:
-        return f"""  * Número de Estados del Sistema: {len(self.state_history)}
+        base_info = f"""  * Número de Estados del Sistema: {len(self.state_history)}
   * Número de Procesos Finalizados: {len(self.finished_processes)}
   * Tiempo de Retorno del Lote: {self.batch_return_time}
   * Tiempo Promedio de Retorno del Lote: {self.average_batch_return_time:.2f}
   * Tiempo de CPU Inactivo: {self.idle_cpu_time}
   * Tiempo de CPU del SO: {self.os_cpu_time}"""
+
+        finished_processes_info = "\n  * Procesos Finalizados:"
+        for i, process in enumerate(self.finished_processes, 1):
+            finished_processes_info += f"""
+    PID: {process.id}. Nombre: {process.name}
+       Tiempo de Llegada: {process.arrival_time}
+       Tiempo de Retorno: {process.return_time}
+       Tiempo de Retorno Normalizado: {process.normalized_return_time:.2f}"""
+
+        return base_info + finished_processes_info
